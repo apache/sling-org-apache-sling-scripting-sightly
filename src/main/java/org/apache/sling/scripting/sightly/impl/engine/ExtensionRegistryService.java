e554fa42c718fa0ee5ff516ad3a4251c70127ca7
Radu Cotescu <radu@apache.org>
2018-10-19 17:14:08 +0200

    SLING-8045 - The ExtensionRegistryService does not correctly manage references
    * work with ServiceReferences only in the unbind method
    * implemented fallback when an extension is unregistered, so that a lower
    priority extension becomes the new active extension for a certain operation
    after a higher priority extension is unregistered

--- src/main/java/org/apache/sling/scripting/sightly/impl/engine/ExtensionRegistryService.java
+++ src/main/java/org/apache/sling/scripting/sightly/impl/engine/ExtensionRegistryService.java
@@ -1,30 +1,32 @@
-/*******************************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- ******************************************************************************/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Licensed to the Apache Software Foundation (ASF) under one
+ ~ or more contributor license agreements.  See the NOTICE file
+ ~ distributed with this work for additional information
+ ~ regarding copyright ownership.  The ASF licenses this file
+ ~ to you under the Apache License, Version 2.0 (the
+ ~ "License"); you may not use this file except in compliance
+ ~ with the License.  You may obtain a copy of the License at
+ ~
+ ~   http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing,
+ ~ software distributed under the License is distributed on an
+ ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ ~ KIND, either express or implied.  See the License for the
+ ~ specific language governing permissions and limitations
+ ~ under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package org.apache.sling.scripting.sightly.impl.engine;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.apache.sling.scripting.sightly.extension.RuntimeExtension;
-import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
@@ -37,7 +39,7 @@ import org.osgi.service.component.annotations.ReferencePolicy;
 public class ExtensionRegistryService {
 
     private volatile Map<String, RuntimeExtension> mapping = new HashMap<>();
-    private Map<String, Integer> mappingPriorities = new HashMap<>(10, 0.9f);
+    private final Map<String, TreeSet<RuntimeExtensionReference>> extensions = new HashMap<>();
 
     public Map<String, RuntimeExtension> extensions() {
         return mapping;
@@ -49,38 +51,47 @@ public class ExtensionRegistryService {
             cardinality = ReferenceCardinality.MULTIPLE
             )
     @SuppressWarnings("unused")
-    protected synchronized void bindExtensionService(RuntimeExtension extension, Map<String, Object> properties) {
-        Integer newPriority = PropertiesUtil.toInteger(properties.get(Constants.SERVICE_RANKING), 0);
-        String extensionName = PropertiesUtil.toString(properties.get(RuntimeExtension.NAME), "");
-        Integer priority = PropertiesUtil.toInteger(mappingPriorities.get(extensionName), 0);
-        if (newPriority > priority) {
-                mapping = Collections.unmodifiableMap(add(mapping, extension, extensionName));
-                mappingPriorities.put(extensionName, newPriority);
-        } else {
-            if (!mapping.containsKey(extensionName)) {
-                mapping = Collections.unmodifiableMap(add(mapping, extension, extensionName));
-                mappingPriorities.put(extensionName, newPriority);
+    protected void bindExtensionService(ServiceReference<RuntimeExtension> serviceReference, RuntimeExtension runtimeExtension) {
+        RuntimeExtensionReference rer = new RuntimeExtensionReference(serviceReference, runtimeExtension);
+        synchronized (extensions) {
+            Set<RuntimeExtensionReference> namedExtensions = extensions.computeIfAbsent(rer.getName(), key -> new TreeSet<>());
+            if (namedExtensions.add(rer)) {
+                mapping = getRuntimeExtensions();
             }
         }
-
     }
 
     @SuppressWarnings("unused")
-    protected synchronized void unbindExtensionService(RuntimeExtension extension, Map<String, Object> properties) {
-        String extensionName = PropertiesUtil.toString(properties.get(RuntimeExtension.NAME), "");
-        mappingPriorities.remove(extensionName);
-        mapping = Collections.unmodifiableMap(remove(mapping, extensionName));
+    protected void unbindExtensionService(ServiceReference<RuntimeExtension> serviceReference) {
+        synchronized (extensions) {
+            String name = PropertiesUtil.toString(serviceReference.getProperty(RuntimeExtension.NAME), "");
+            Set<RuntimeExtensionReference> namedExtensions = extensions.get(name);
+            boolean changed = false;
+            if (namedExtensions != null) {
+                for (RuntimeExtensionReference runtimeExtensionReference : namedExtensions) {
+                    if (serviceReference.equals(runtimeExtensionReference.getServiceReference())) {
+                        namedExtensions.remove(runtimeExtensionReference);
+                        if (namedExtensions.isEmpty()) {
+                            extensions.remove(name);
+                        }
+                        changed = true;
+                        break;
+                    }
+                }
+            }
+            if (changed) {
+                mapping = getRuntimeExtensions();
+            }
+        }
     }
 
-    private Map<String, RuntimeExtension> add(Map<String, RuntimeExtension> oldMap, RuntimeExtension extension, String extensionName) {
-        HashMap<String, RuntimeExtension> newMap = new HashMap<>(oldMap);
-        newMap.put(extensionName, extension);
-        return newMap;
+    private Map<String, RuntimeExtension> getRuntimeExtensions() {
+        HashMap<String, RuntimeExtension> replacement = new HashMap<>();
+        for (Map.Entry<String, TreeSet<RuntimeExtensionReference>> entry : extensions.entrySet()) {
+            replacement.put(entry.getKey(), entry.getValue().last().getService());
+        }
+        return Collections.unmodifiableMap(replacement);
     }
 
-    private Map<String, RuntimeExtension> remove(Map<String, RuntimeExtension> oldMap, String extensionName) {
-        HashMap<String, RuntimeExtension> newMap = new HashMap<>(oldMap);
-        newMap.remove(extensionName);
-        return newMap;
-    }
+
 }
